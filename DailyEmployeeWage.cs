2c560f5d5c4963f251e79bf7b759ec5f36618f4b
kundan <goswamikundan123@gmail.com>
2023-02-17 11:18:53 +0530

    Adding file which computes Daily Employee Wage and displays it.

---
+++ DailyEmployeeWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationOOPS
+{
+    public class DailyEmployeeWage
+    {
+        public void Employeewage()
+        {
+            int Is_full_time = 1;
+            int Emp_rate_per_hour = 20;
+
+            int empHrs = 0;
+            int empWages = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == Is_full_time)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWages = empHrs * Emp_rate_per_hour;
+            Console.WriteLine("Employee Wage = " + empWages);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@
         {
             Console.WriteLine("Welcome to Employee Wage Computation.");
             
-            CheckEmployee employee = new CheckEmployee();
-            employee.EmployeeCheck();
+            //CheckEmployee employee = new CheckEmployee();
+            //employee.EmployeeCheck();
+
+            DailyEmployeeWage day = new DailyEmployeeWage();
+            day.Employeewage();
         }
     }
 }
