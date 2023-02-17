8061ac3e290c7dd90b561f742f314829cf40c064
kundan <goswamikundan123@gmail.com>
2023-02-17 11:09:59 +0530

    Adding file which displays Employee is present or absent.

---
+++ CheckEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationOOPS
+{
+    public class CheckEmployee
+    {
+        public void EmployeeCheck()
+        {
+            int Is_full_time = 1;
+            Random random = new Random ();
+            int empCheck = random.Next(2);
+
+            if (empCheck == Is_full_time) 
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation.");
+            
+            CheckEmployee employee = new CheckEmployee();
+            employee.EmployeeCheck();
         }
     }
 }
